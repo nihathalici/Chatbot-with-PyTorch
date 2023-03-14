4128d1940840d9b89d602943e3faa9e55c1c0b01
Nihat Halici <nihat.halici@gmail.com>
2023-03-14 21:43:14 +0100

    Add files via upload

---
+++ Part-01/nltk_utils.py
@@ -0,0 +1,26 @@
+import nltk
+from nltk.stem.porter import PorterStemmer
+
+stemmer = PorterStemmer()
+
+
+def tokenize(sentence):
+    return nltk.word_tokenize(sentence)
+
+
+def stem(word):
+    return stemmer.stem(word.lower())
+
+
+def bag_of_words(tokenized_sentence, all_words):
+    pass
+
+
+# a = "How long does shipping take?"
+# print(a)
+# a = tokenize(a)
+# print(a)
+
+words = ["Organize", "organizes", "organizing"]
+stemmed_words = [stem(w) for w in words]
+print(stemmed_words)
